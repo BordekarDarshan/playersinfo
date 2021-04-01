b8d8036538d0f9f8d9e6cb8a7ec79de3511aa95e
darshan bordekar <darshanbordekar1819@gmail.com>
2021-04-02 02:11:38 +0530

    [ Darshan ] | Commeted custom cursor UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState /*useRef, useEffect*/ } from "react";
 import { ThemeProvider } from "styled-components";
 import Footer from "./Components/Footer/Footer.jsx";
 import Navigation from "./Components/Navigation/Navigation.jsx";
 import Home from "./Page/Home.jsx";
-import { GlobalStyles, Cursor } from "./Theme/Global.magic.jsx";
+import { GlobalStyles /*Cursor*/ } from "./Theme/Global.magic.jsx";
 import { lightTheme, darkTheme } from "./Theme/Theme";
 
 function App() {
   const [theme, setTheme] = useState("light");
 
-  let cursor = useRef();
+  /* let cursor = useRef();
   useEffect(() => {
     window.addEventListener("mousemove", cursorLocate);
   }, []);
@@ -17,7 +17,7 @@ function App() {
   let cursorLocate = (e) => {
     cursor.current.style.top = e.pageY + "px";
     cursor.current.style.left = e.pageX + "px";
-  };
+  } */
 
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
@@ -26,7 +26,7 @@ function App() {
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <Cursor ref={cursor}></Cursor>
+      {/* <Cursor ref={cursor}></Cursor> */}
       <Navigation changeMode={themeToggler} theme={theme} />
       <Home theme={theme} />
       <Footer />
