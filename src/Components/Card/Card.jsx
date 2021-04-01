e37854df3578ef7c64a8ca15d308b20aed310fa2
darshan bordekar <darshanbordekar1819@gmail.com>
2021-04-02 00:04:25 +0530

    [ Darshan ] | Handled empty string response

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,8 +8,12 @@ export class Card extends Component {
     window.open(url, "_blank");
   };
   timeZoneHandler = (date) => {
-    const d1 = new Date(date);
-    return d1.toString();
+    if (date?.length === 0) {
+      return "Soon";
+    } else {
+      const d1 = new Date(date);
+      return d1.toString();
+    }
   };
 
   render() {
@@ -25,7 +29,11 @@ export class Card extends Component {
           <span>{data.PFName}</span>
           <span>{data.SkillDesc}</span>
           <span>{`$ ${data.Value}`}</span>
-          <span>{`${data.CCode} Vs ${data.UpComingMatchesList[0].VsCCode}`}</span>
+          <span>{`${data.CCode} Vs ${
+            data.UpComingMatchesList[0].VsCCode.length === 0
+              ? "?"
+              : data.UpComingMatchesList[0].VsCCode
+          }`}</span>
           <span>{this.timeZoneHandler(data.UpComingMatchesList[0].MDate)}</span>
         </Navigate>
       </Container>
